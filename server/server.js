35a3cc4fbd3c51d9de299dab84d3f5e9cbea3cfe
ocean <osciris.veliz@gmail.com>
2022-10-15 14:43:13 -0500

    build query type definitions and resolvers

--- server/server.js
+++ server/server.js
@@ -1,14 +1,37 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+// import typeDefs and resolvers from the schemas directory
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+// creates a new Apollo server and passes in the schema datea
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`🚀 API server running on port ${PORT}!`);
+// Create a new instance of an Apollo Server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+
+  // integrate the Apollo server with the express application as middleware
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🚀 API server running on port ${PORT}!`);
+      // log where we can go to test the GQL API
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
   });
-});
+};
+
+//Call to the async function to start the server
+startApolloServer(typeDefs, resolvers);
