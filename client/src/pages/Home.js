6fcaa9458f5df9c8ef9cb03db3a1bb52c3911f54
ocean <osciris.veliz@gmail.com>
2022-10-16 17:07:01 -0500

    create ThoughtList component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';
+import ThoughtList from '../components/ThoughtList';
 
 const Home = () => {
+  // use useQuery hook to make query request
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  // LOADING: @apollo/client lib provides this propery to indicate request isn't done yet
+  // when finished/data retunred from server, the info is stored in the destructured DATA propery
+  const thoughts = data?.thoughts || [];
+  console.log('thoughts from data:', thoughts);
+
   return (
     <main>
       <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+        <div className='col-12 mb-3'>
+          {loading ? (
+          <div>Loading...</div> 
+          ) : (
+            <ThoughtList thoughts={thoughts} title='Some Feed for Thought(s)...' />
+
+          )}  
+        </div>
       </div>
     </main>
   );
