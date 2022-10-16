bcd43b20873552c7065c07693b28f4511ead7fc0
ocean <osciris.veliz@gmail.com>
2022-10-16 18:34:58 -0500

    set up the main URL routes using react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 // ApolloProvider: a special type of React comp. that provides data to all other compon.
 // ApolloClient: constructor function that helps initialize connection to the GraphQL API server
 // InMemoryCache: enables Apollo Client instance to cache API response data for more efficient perfomance
 // createHttpLink: allows control on how the Apollo client makes a request. Like middleware for outbound network requests
 
-
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -23,14 +28,22 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/signup' element={<Signup />} />
+              <Route path='/profile' element={<Profile />} />
+              <Route path='/thought' element={<SingleThought />} />
+              <Route path='/*' element={<NoMatch />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-
+      </Router>
     </ApolloProvider>
   );
 }
